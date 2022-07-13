e232bbfdccb07ae2ba1dfcde33372f1d53d04f5e
Charlie Zhao <charles.zhao5461@gmail.com>
2022-07-14 04:06:36 +1000

    Refactor into CartItem

---
+++ src/components/cart/cart-item.tsx
@@ -0,0 +1,51 @@
+import styled from "@emotion/styled";
+import Image from "../image";
+
+const CartItem = () => {
+  return (
+    <CartItemWrapper>
+      <Image src="/images/Bill2.png" />
+      <CartItemBody>
+        <strong>Viktor Tee</strong>
+        <strong>$70 AUD</strong>
+        <CartItemBodySummary>
+          <div>
+            <strong>Color:</strong> <span>White</span>
+          </div>
+          <div>
+            <strong>Size:</strong> <span>S</span>
+          </div>
+          <div>
+            <strong>Quantity:</strong> <span>1</span>
+          </div>
+        </CartItemBodySummary>
+        <span>Remove</span>
+      </CartItemBody>
+    </CartItemWrapper>
+  );
+};
+
+export default CartItem;
+
+const CartItemWrapper = styled.div`
+  width: calc(50% - 1rem);
+  height: min-content;
+  margin: 1rem 1rem 0 0;
+
+  @media screen and (min-width: 650px) {
+    width: calc(20% - 1rem);
+  }
+
+  & > div:first-of-type {
+    border-radius: 1.5rem;
+    overflow: hidden;
+  }
+`;
+const CartItemBody = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: var(--gap-l);
+`;
+const CartItemBodySummary = styled.div`
+  margin: var(--gap-l) 0;
+`;
--- src/components/cart/cart.tsx
+++ src/components/cart/cart.tsx
@@ -2,8 +2,8 @@ import styled from "@emotion/styled";
 import { motion, AnimatePresence } from "framer-motion";
 import { useState } from "react";
 import { StyledButton } from "../shared-styles/styled-button";
-import Image from "../image";
 import Accordion from "../accordion/accordion";
+import CartItem from "./cart-item";
 
 const Cart = () => {
   const [cartOpen, setCartOpen] = useState(false);
@@ -51,32 +51,10 @@ const Cart = () => {
             exit="exit"
             animate="animate"
           >
-            <CartItem>
-              <Image src="/images/Bill2.png" />
-              <CartItemBody>
-                <strong>Viktor Tee</strong>
-                <strong>$70 AUD</strong>
-                <CartItemBodySummary>
-                  <div>
-                    <strong>Color:</strong> <span>White</span>
-                  </div>
-                  <div>
-                    <strong>Size:</strong> <span>S</span>
-                  </div>
-                  <div>
-                    <strong>Quantity:</strong> <span>1</span>
-                  </div>
-                </CartItemBodySummary>
-                <span>Remove</span>
-              </CartItemBody>
-            </CartItem>
+            <CartItemWrapper>
+              <CartItem />
+            </CartItemWrapper>
             <Accordion options={accordionOptions} />
-            {/* <TempFillerText>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio
-              blanditiis consectetur similique porro architecto cupiditate cum
-              eligendi est dolorem, facere itaque consequuntur molestiae
-              obcaecati repellat neque, libero quae culpa recusandae!
-            </TempFillerText> */}
           </CartModal>
         )}
       </AnimatePresence>
@@ -105,29 +83,7 @@ const CartModal = styled(motion.div)`
   backdrop-filter: blur(3px) invert(1);
   padding: var(--gap-page-top) var(--gap-xxl);
 `;
-const CartItem = styled.div`
-  width: 50%;
-  height: min-content;
-
-  @media screen and (min-width: 650px) {
-    width: 25%;
-  }
-
-  & > div:first-of-type {
-    border-radius: 1.5rem;
-    overflow: hidden;
-  }
-`;
-const CartItemBody = styled.div`
+const CartItemWrapper = styled.div`
   display: flex;
-  flex-direction: column;
-  margin-top: var(--gap-l);
-`;
-const CartItemBodySummary = styled.div`
-  margin: var(--gap-l) 0;
-`;
-const TempFillerText = styled.div`
-  /* border-top: 1px solid gray; */
-  margin-top: 1rem;
-  padding-top: 1rem;
+  flex-wrap: wrap;
 `;
