96ac85b5b63e0e2d9d8ed184f4d1e4ef7a7d98d1
Charlie Zhao <charles.zhao5461@gmail.com>
2022-07-20 15:48:36 +1000

    Settings for styled components

--- next.config.js
+++ next.config.js
@@ -6,6 +6,24 @@ const nextConfig = {
 // module.exports = nextConfig;
 
 module.exports = {
+  compiler: {
+    // see https://styled-components.com/docs/tooling#babel-plugin for more info on the options.
+    styledComponents: {
+      displayName: true,
+      ssr: true,
+      fileName: true,
+      topLevelImportPaths: [],
+      meaninglessFileNames: [],
+      cssProp: true,
+      namespace: "",
+      // Not supported yet.
+      minify: true,
+      // Not supported yet.
+      transpileTemplateLiterals: true,
+      // Not supported yet.
+      pure: true,
+    },
+  },
   reactStrictMode: true,
   images: {},
 };
