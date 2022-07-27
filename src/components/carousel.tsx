4710d2d0584c13f16e83b22df39b819999836e9e
Charlie Zhao <charles.zhao5461@gmail.com>
2022-07-28 08:40:53 +1000

    Clean up and improve framer transitions for carousel

--- src/components/carousel.tsx
+++ src/components/carousel.tsx
@@ -5,6 +5,7 @@ import { mediaByIndex } from "../../public/images/carousel-images";
 import { StyledPageWrapperCentered } from "./shared-styles/styled-page-wrapper";
 import Image from "./image";
 import { default as NextImage } from "next/image";
+import { motion, AnimatePresence } from "framer-motion";
 
 // TODO: https://www.embla-carousel.com/plugins/wheel-gestures/
 type Props = {
@@ -13,15 +14,16 @@ type Props = {
 
 const Carousel = ({ slides }: Props) => {
   const [viewportRef, embla] = useEmblaCarousel({
-    // dragFree: true,
     containScroll: "trimSnaps",
   });
 
+  // TODO: Can be used to gray out next/prev buttons when reach the start/end
   const [prevBtnEnabled, setPrevBtnEnabled] = useState(false);
   const [nextBtnEnabled, setNextBtnEnabled] = useState(false);
 
   const scrollPrev = useCallback(() => embla && embla.scrollPrev(), [embla]);
   const scrollNext = useCallback(() => embla && embla.scrollNext(), [embla]);
+
   const onSelect = useCallback(() => {
     if (!embla) return;
     setPrevBtnEnabled(embla.canScrollPrev());
@@ -34,29 +36,51 @@ const Carousel = ({ slides }: Props) => {
     onSelect();
   }, [embla, onSelect]);
 
+  const parentVariants = {
+    hidden: {
+      translateX: "var(--translate-distance)",
+    },
+    show: {
+      translateX: 0,
+      transition: {
+        duration: 1,
+        type: "spring",
+      },
+    },
+  };
+
   return (
     <StyledPageWrapperCentered>
+      <ButtonContainer>
+        <button onClick={scrollPrev}>Prev</button>
+        <button onClick={scrollNext}>Next</button>
+      </ButtonContainer>
       <Embla>
         {/* what's viewport ref? */}
         <EmblaViewPort ref={viewportRef}>
-          <EmblaContainer>
-            {slides.map((index) => {
-              const { width, height } = mediaByIndex(index);
-              console.log(`w: ${width}, h: ${height}`);
-
-              return (
-                <EmblaSlide
-                  key={index}
-                  style={{ aspectRatio: `${width} / ${height}` }}
-                >
-                  {index === 0 && (
-                    <SlideText>Oslo 52.3676° N, 4.9041° E</SlideText>
-                  )}
-                  <img src={mediaByIndex(index).src} alt="" />
-                </EmblaSlide>
-              );
-            })}
-          </EmblaContainer>
+          <AnimatePresence>
+            <EmblaContainer
+              variants={parentVariants}
+              initial="hidden"
+              animate="show"
+              exit="hidden"
+            >
+              {slides.map((index) => {
+                const { width, height } = mediaByIndex(index);
+                return (
+                  <EmblaSlide
+                    key={index}
+                    style={{ aspectRatio: `${width} / ${height}` }}
+                  >
+                    {index === 0 && (
+                      <SlideText>Oslo 52.3676° N, 4.9041° E</SlideText>
+                    )}
+                    <img src={mediaByIndex(index).src} alt="" />
+                  </EmblaSlide>
+                );
+              })}
+            </EmblaContainer>
+          </AnimatePresence>
         </EmblaViewPort>
       </Embla>
       <FlexRow>
@@ -82,21 +106,26 @@ const Carousel = ({ slides }: Props) => {
 
 export default Carousel;
 
-// TO-DO: currently styled up to here,
-//        please understand what each style is doing after finished
+// TODO: Please understand what each style is doing after finished
 const Embla = styled.div`
   padding: var(--gap-xs); // creates the white border effect
 `;
-const EmblaViewPort = styled.div`
+const EmblaViewPort = styled(motion.div)`
   overflow: hidden;
   padding-top: var(--gap-l); // creates space for the slide text
 `;
-const EmblaContainer = styled.div`
+const EmblaContainer = styled(motion.div)`
   display: flex;
   user-select: none;
   height: 50vh;
+
+  --translate-distance: -900px;
+
+  @media screen and (min-width: 650px) {
+    --translate-distance: -1200px;
+  }
 `;
-const EmblaSlide = styled.div`
+const EmblaSlide = styled(motion.div)`
   transition: min-width 1s ease;
   cursor: grab;
   margin-right: var(--gap-xs); // gap between slides
@@ -127,4 +156,7 @@ const FlexRow = styled.div`
   & > div:nth-of-type(2) {
     text-align: right;
   }
+`;
+const ButtonContainer = styled(FlexRow)`
+  max-width: 9rem;
 `;
