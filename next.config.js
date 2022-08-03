c4148a880563cb2bdad873722c7029f3dbe80eb9
Charlie Zhao <charles.zhao5461@gmail.com>
2022-08-03 22:43:43 +1000

    Change to yarn and fix next.config

--- next.config.js
+++ next.config.js
@@ -1,24 +1,7 @@
 /** @type {import('next').NextConfig} */
 
 module.exports = {
-  compiler: {
-    // see https://styled-components.com/docs/tooling#babel-plugin for more info on the options.
-    styledComponents: {
-      displayName: true,
-      ssr: true,
-      fileName: true,
-      topLevelImportPaths: [],
-      meaninglessFileNames: [],
-      cssProp: true,
-      namespace: "",
-      // Not supported yet.
-      minify: true,
-      // Not supported yet.
-      transpileTemplateLiterals: true,
-      // Not supported yet.
-      pure: true,
-    },
-  },
+  compiler: {},
   reactStrictMode: true,
   images: {
     domains: ["cdn.sanity.io"],
