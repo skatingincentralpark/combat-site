0d3c3e2a3f95fc37cb23a6dcbffc1521fa315fc3
Charlie Zhao <charles.zhao5461@gmail.com>
2022-08-06 13:04:30 +1000

    Add back StaticImageData type

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,3 +1,17 @@
+// TO-DO: Remove when Next provides this type by default, research why it's not working
+
+type StaticImageData = {
+  src: string;
+  height: number;
+  width: number;
+  blurDataURL?: string;
+};
+
+declare module "*.gif" {
+  const content: StaticImageData;
+  export default content;
+}
+
 type ImageType = {
   image: {
     aspectRatio: number;
