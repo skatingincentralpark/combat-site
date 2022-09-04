e1646c083b4fa09c43902e79f0fedc0bb41e9cad
Charlie Zhao <charles.zhao5461@gmail.com>
2022-09-04 20:40:09 +1000

    Remove color strips from home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,13 @@
 import styled from "@emotion/styled";
-import ColorStrips from "../components/color-strips";
 import { StyledPageWrapper } from "../components/shared-styles/styled-page-wrapper";
 import BillEvans from "../components/bill-evans";
 
 const HomePage = () => {
-  const chosenColors = [
-    "var(--green-2)",
-    "var(--green-3)",
-    "var(--yellow-1)",
-    "var(--yellow-2)",
-    "var(--olive-1)",
-    "red",
-    "orange",
-  ];
-
-  const colors = [...chosenColors];
-
   return (
     <StyledPageWrapper>
       <BillWrapper>
         <BillEvans />
       </BillWrapper>
-      {/* <ColorStrips colors={colors} /> */}
     </StyledPageWrapper>
   );
 };
