b3c23e00e800b27707449dfbe612d3b8912640ed
Charlie Zhao <charles.zhao5461@gmail.com>
2022-09-17 08:35:59 +1000

    Refactor app and move page-transition-wrapper to own component

--- src/components/page-transition-wrapper.tsx
+++ src/components/page-transition-wrapper.tsx
@@ -1,13 +1,15 @@
-import React from "react";
+import { default as NextImage } from "next/image";
 import { useRouter } from "next/router";
 import styled from "@emotion/styled";
-import { m, AnimatePresence } from "framer-motion";
+import { AnimatePresence, m } from "framer-motion";
+import { pageTransitionSpeed } from "@lib/animate";
 
 type Props = {
   children: React.ReactNode;
+  loading: boolean;
 };
 
-const PageTransitionWrapper = ({ children }: Props) => {
+const PageTransitionWrapper = ({ children, loading }: Props) => {
   const router = useRouter();
 
   const variants = {
@@ -22,20 +24,57 @@ const PageTransitionWrapper = ({ children }: Props) => {
     },
     exit: {
       opacity: 0,
+      transition: {
+        duration: 0.4,
+      },
     },
   };
 
   return (
     <AnimatePresence exitBeforeEnter>
-      <StyledMotionDiv
-        key={router.pathname}
-        variants={variants}
-        initial="initial"
-        exit="exit"
-        animate="animate"
-      >
-        {children}
-      </StyledMotionDiv>
+      {loading && (
+        <ImageWrapper
+          initial={{
+            opacity: 0,
+            x: "-200%",
+            y: "-50%",
+          }}
+          animate={{
+            opacity: 1,
+            x: "-50%",
+            y: "-50%",
+            transition: {
+              opacity: { duration: pageTransitionSpeed / 1000 },
+            },
+          }}
+          exit={{
+            opacity: 0,
+            x: "200%",
+            y: "-50%",
+            transition: {
+              opacity: { duration: 0.2 },
+            },
+          }}
+        >
+          <NextImage
+            src="/images/otter-cheeks.jpeg"
+            alt="Something"
+            layout="fill"
+            objectFit="cover"
+          />
+        </ImageWrapper>
+      )}
+      {!loading && (
+        <StyledMotionDiv
+          key={router.pathname}
+          variants={variants}
+          initial="initial"
+          exit="exit"
+          animate="animate"
+        >
+          {children}
+        </StyledMotionDiv>
+      )}
     </AnimatePresence>
   );
 };
@@ -45,4 +84,26 @@ export default PageTransitionWrapper;
 const StyledMotionDiv = styled(m.div)`
   height: 100%;
   width: 100%;
+`;
+const ImageWrapper = styled(m.div)`
+  height: 300px;
+  width: 300px;
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+
+  animation: IAmDeeCaptunNow 1s infinite;
+
+  @keyframes IAmDeeCaptunNow {
+    0% {
+      filter: invert(0);
+    }
+    50% {
+      filter: invert(1);
+    }
+    100% {
+      filter: invert(0);
+    }
+  }
 `;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,13 @@
 import { useEffect, useState } from "react";
-import { useRouter } from "next/router";
 import Router from "next/router";
-import styled from "@emotion/styled";
-import { LazyMotion, domAnimation, AnimatePresence, m } from "framer-motion";
+import { LazyMotion, domAnimation } from "framer-motion";
 import type { AppProps } from "next/app";
 import GlobalStyles from "../styles/global";
-import Header from "../components/header";
-import Announcement from "../components/announcement";
+import Header from "components/header/header";
+import Announcement from "components/announcement";
 import { deviceIsBrowser } from "@lib/helpers";
 import { pageTransitionSpeed } from "@lib/animate";
-import { default as NextImage } from "next/image";
+import PageTransitionWrapper from "components/page-transition-wrapper";
 
 if (deviceIsBrowser) {
   console.log(`Greetings, Traveller 🦧`);
@@ -58,105 +56,3 @@ function MyApp({ Component, pageProps }: AppProps) {
 }
 
 export default MyApp;
-
-type Props = {
-  children: React.ReactNode;
-  loading: boolean;
-};
-
-const PageTransitionWrapper = ({ children, loading }: Props) => {
-  const router = useRouter();
-
-  const variants = {
-    initial: {
-      opacity: 0,
-    },
-    animate: {
-      opacity: 1,
-      transition: {
-        duration: 0.8,
-      },
-    },
-    exit: {
-      opacity: 0,
-      transition: {
-        duration: 0.4,
-      },
-    },
-  };
-
-  return (
-    <AnimatePresence exitBeforeEnter>
-      {loading && (
-        <ImageWrapper
-          initial={{
-            opacity: 0,
-            x: "-200%",
-            y: "-50%",
-          }}
-          animate={{
-            opacity: 1,
-            x: "-50%",
-            y: "-50%",
-            transition: {
-              opacity: { duration: pageTransitionSpeed / 1000 },
-            },
-          }}
-          exit={{
-            opacity: 0,
-            x: "200%",
-            y: "-50%",
-            transition: {
-              opacity: { duration: 0.2 },
-            },
-          }}
-        >
-          <NextImage
-            src="/images/otter-cheeks.jpeg"
-            alt="Something"
-            layout="fill"
-            objectFit="cover"
-          />
-        </ImageWrapper>
-      )}
-      {!loading && (
-        <StyledMotionDiv
-          key={router.pathname}
-          variants={variants}
-          initial="initial"
-          exit="exit"
-          animate="animate"
-        >
-          {children}
-        </StyledMotionDiv>
-      )}
-    </AnimatePresence>
-  );
-};
-
-const StyledMotionDiv = styled(m.div)`
-  height: 100%;
-  width: 100%;
-`;
-const ImageWrapper = styled(m.div)`
-  height: 300px;
-  width: 300px;
-  position: fixed;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-
-  animation: IAmDeeCaptunNow 1s infinite;
-
-  @keyframes IAmDeeCaptunNow {
-    0% {
-      filter: invert(0);
-    }
-    50% {
-      filter: invert(1);
-    }
-    100% {
-      filter: invert(0);
-    }
-  }
-`;
