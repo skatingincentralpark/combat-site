ed647de2fdcdc7f0df9758c3f5641f76525e8957
Charlie Zhao <charles.zhao5461@gmail.com>
2022-09-18 21:56:39 +1000

    Add framer for menu animation, change chevron to rotate animation

--- src/components/dropdown.tsx
+++ src/components/dropdown.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import styled from "@emotion/styled";
 import { useState } from "react";
+import { m, AnimatePresence } from "framer-motion";
 
 interface Item {
   id: number;
@@ -14,6 +15,19 @@ interface DropdownProps {
   defaultTitle?: string;
   setList: React.Dispatch<React.SetStateAction<Item[]>>;
 }
+
+const dropdownVariants = {
+  collapsed: {
+    opacity: 0,
+    height: "0rem",
+    transition: { duration: 0.6, ease: [0.04, 0.62, 0.23, 0.98] },
+  },
+  open: {
+    opacity: 1,
+    height: "auto",
+    transition: { duration: 0.6, ease: [0.04, 0.62, 0.23, 0.98] },
+  },
+};
 
 const Dropdown = ({
   list,
@@ -59,21 +73,29 @@ const Dropdown = ({
         <DdHeaderTitle>
           {headerTitle} {emoji}
         </DdHeaderTitle>
-        {listOpen ? <Chevron direction="up" /> : <Chevron direction="down" />}
+        <Chevron listOpen={listOpen} />
       </DdHeader>
-      {listOpen && (
-        <DdList role="list">
-          {list.map((item) => (
-            <DdListItem
-              type="button"
-              key={item.title}
-              onClick={() => selectItem(item)}
-            >
-              {item.selected && "✅"} {item.title}
-            </DdListItem>
-          ))}
-        </DdList>
-      )}
+      <AnimatePresence>
+        {listOpen && (
+          <DdList
+            role="list"
+            variants={dropdownVariants}
+            initial="collapsed"
+            animate="open"
+            exit="collapsed"
+          >
+            {list.map((item) => (
+              <DdListItem
+                type="button"
+                key={item.title}
+                onClick={() => selectItem(item)}
+              >
+                {item.selected && "✅"} {item.title}
+              </DdListItem>
+            ))}
+          </DdList>
+        )}
+      </AnimatePresence>
     </DdWrapper>
   );
 };
@@ -87,29 +109,41 @@ const DdWrapper = styled.div`
 `;
 const DdHeader = styled.button<{ listOpen: boolean }>`
   width: 100%;
+  z-index: 3;
   display: flex;
   align-items: center;
   justify-content: space-between;
   position: relative;
-  border: 1px solid rgb(223, 223, 223);
   border: 1px solid var(--gray-2);
   border-radius: ${({ listOpen }) =>
     listOpen ? "var(--gap-3xs) var(--gap-3xs) 0 0" : "var(--gap-3xs)"};
   background-color: #fff;
-  cursor: pointer;
+  cursor: s-resize;
   padding: var(--gap-4xs) var(--gap-3xs);
+  transition: border-radius 1000ms ease;
+
+  &:active {
+    border-color: magenta;
+  }
+
+  & > * {
+    transition: scale 100ms;
+    transform-origin: left;
+  }
+  &:active > * {
+    scale: 0.97;
+  }
 `;
 const DdHeaderTitle = styled.div`
   width: 90%;
   flex-shrink: 0;
   text-align: left;
 `;
-const DdList = styled.div`
+const DdList = styled(m.div)`
   position: absolute;
   z-index: 2;
   width: 100%;
   max-height: 10rem;
-  border: 1px solid rgb(223, 223, 223);
   border: 1px solid var(--gray-2);
   border-top: none;
   border-radius: 0 0 var(--gap-3xs) var(--gap-3xs);
@@ -147,32 +181,26 @@ const DdListItem = styled.button`
   }
 `;
 
-interface ChevronProps {
-  direction?: "up" | "down";
-}
-
-const Chevron = ({ direction = "up" }: ChevronProps) => {
-  console.log(direction);
+const Chevron = ({ listOpen = false }) => {
   return (
-    <StyledChevronWrapper>
-      {direction !== "up" ? (
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-          {/* Font Awesome Pro 6.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. */}
-          <path d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z" />
-        </svg>
-      ) : (
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-          {/* Font Awesome Pro 6.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. */}
-          <path d="M233.4 105.4c12.5-12.5 32.8-12.5 45.3 0l192 192c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L256 173.3 86.6 342.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l192-192z" />
-        </svg>
-      )}
+    <StyledChevronWrapper listOpen={listOpen}>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+        {/* Font Awesome Pro 6.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. */}
+        <path d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z" />
+      </svg>
     </StyledChevronWrapper>
   );
 };
 
-const StyledChevronWrapper = styled.span`
+const StyledChevronWrapper = styled.span<{ listOpen: boolean }>`
   height: 100%;
+  width: 100%;
+  aspect-ratio: 1;
   padding: 0.1rem;
+  transition: transform 200ms ease;
+  transform-origin: center;
+  transform: ${({ listOpen }) =>
+    listOpen ? "rotate(180deg)" : "rotate(0deg)"};
 
   & > svg {
     height: 100%;
