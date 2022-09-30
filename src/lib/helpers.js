7dfe9a42565bf0df138446af0e4feeafa0529219
Charlie Zhao <charles.zhao5461@gmail.com>
2022-09-30 22:58:10 +1000

    Lookbook Index POC - For fun

--- src/lib/helpers.js
+++ src/lib/helpers.js
@@ -1,1 +1,12 @@
 export const deviceIsBrowser = typeof window !== "undefined";
+
+export const slugToText = (slug) => {
+  if (!slug) return;
+  return slug
+    .split("-")
+    .join(" ")
+    .replace(
+      /\w\S*/g,
+      (txt) => txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase()
+    );
+};
--- src/pages/lookbooks/index.tsx
+++ src/pages/lookbooks/index.tsx
@@ -1,27 +1,132 @@
-import Link from "next/link";
+import { useRouter } from "next/router";
 import client from "../../../client";
-import { StyledPageWrapperCentered } from "@components/shared-styles/page-wrappers";
 import HeadSEO from "@components/head-seo";
+import styled from "@emotion/styled";
+import { slugToText } from "@lib/helpers";
 
 type Props = {
   slugs: string[];
 };
 
 const Index = ({ slugs }: Props) => {
+  const arr = slugs.map((x, i) => {
+    return {
+      slug: x,
+      date: 2021 + i,
+      color: i === 0 ? `#ff579f` : `#bbdfc5`,
+    };
+  });
+
+  const dummyArr = [...Array(30 - slugs.length)];
+
+  const router = useRouter();
+
   return (
-    <StyledPageWrapperCentered px="m">
+    <PageWrapper>
       <HeadSEO title="Lookbooks" />
-      {!!slugs &&
-        slugs.map((slug, i) => (
-          <div key={i}>
-            <Link href={`lookbooks/${slug}`}>{slug}</Link>
-          </div>
+      <LookbookList>
+        {arr.map((item, i) => (
+          <ListItem key={item.slug} borderColor={item.color}>
+            <button onClick={() => router.push(`lookbooks/${item.slug}`)}>
+              <span>{`0${i}`}</span>
+              <span>{slugToText(item.slug)}</span>
+              <span>{item.date}</span>
+            </button>
+          </ListItem>
         ))}
-    </StyledPageWrapperCentered>
+
+        {dummyArr.map((_, i) => (
+          <DummyListItem key={i}>
+            <button>
+              <span />
+              <span>null</span>
+              <span>null</span>
+            </button>
+          </DummyListItem>
+        ))}
+      </LookbookList>
+    </PageWrapper>
   );
 };
 
 export default Index;
+
+const PageWrapper = styled.div`
+  padding-top: var(--gap-page-top);
+  max-height: 100%;
+  overflow: hidden;
+`;
+
+const LookbookList = styled.ul`
+  padding: 0;
+
+  & > li {
+    & > button {
+      display: block;
+      padding: var(--gap-3xs) var(--gap-l);
+      /* font-family: "Bitcount Mono Single Lt Circle", "Courier New", Courier,
+        monospace;
+      -webkit-font-smoothing: initial; */
+      font-weight: 600;
+      font-size: 14px;
+      width: 100%;
+
+      display: grid;
+      grid-template-columns: 2rem minmax(10rem, 30rem) 3fr;
+      grid-gap: var(--gap-s);
+      text-align: left;
+
+      & > span:nth-last-of-type(2) {
+        white-space: nowrap;
+        overflow: hidden;
+        text-overflow: ellipsis;
+      }
+    }
+  }
+
+  & > li:first-of-type {
+    border-top: 0.5px solid var(--gray-4);
+  }
+`;
+
+const ListItem = styled.li<{ borderColor: string }>`
+  border-bottom: 0.5px solid var(--gray-4);
+  list-style: none;
+  border-left: 0.8rem solid var(--gray-2);
+  border-left-color: ${({ borderColor }) => borderColor && borderColor};
+
+  &:hover {
+    @media screen and (min-width: 700px) {
+      background-color: black;
+      color: white;
+    }
+  }
+
+  &:active {
+    background-color: var(--yellow-1);
+  }
+
+  & > button {
+    cursor: pointer;
+  }
+`;
+const DummyListItem = styled.li`
+  border-bottom: 0.5px solid var(--gray-4);
+  list-style: none;
+  border-left: 0.8rem solid var(--gray-2);
+  color: var(--gray-2);
+
+  &:hover {
+    @media screen and (min-width: 700px) {
+      color: var(--gray-3);
+      border-left-color: var(--gray-3);
+    }
+  }
+
+  & > button {
+    cursor: not-allowed;
+  }
+`;
 
 export async function getStaticProps() {
   const slugs = await client.fetch(`*[_type == "lookbook"][].slug.current`);
@@ -32,3 +137,5 @@ export async function getStaticProps() {
     },
   };
 }
+
+const Row = ({ arr }: { arr: [{ slug: string; date: number }] }) => {};
