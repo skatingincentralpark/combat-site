7e081795d907bf13b239cd6ff306da2011c0c66b
Charlie Zhao <charles.zhao5461@gmail.com>
2022-11-01 14:14:14 +1100

    Dont show backButton if page isLoading and add lookbook path

--- src/components/header/backButton.tsx
+++ src/components/header/backButton.tsx
@@ -4,10 +4,17 @@ import { useRouter } from "next/router";
 import { AnimatePresence, m } from "framer-motion";
 import { headerBackButtonVariants } from "@lib/animate";
 
-const BackButton = ({ navOpen }: { navOpen: boolean }) => {
+const BackButton = ({
+  navOpen,
+  isLoading,
+}: {
+  navOpen: boolean;
+  isLoading: boolean;
+}) => {
   const router = useRouter();
   const checkIfShopPath = () => router.pathname.includes("/shop/");
   const checkIfNewsPath = () => router.pathname.includes("/news/");
+  const checkIfLookbookPath = () => router.pathname.includes("/lookbooks/");
 
   const [isShopPath, setIsShopPath] = useState<boolean | null>(
     checkIfShopPath || null
@@ -15,18 +22,25 @@ const BackButton = ({ navOpen }: { navOpen: boolean }) => {
   const [isNewsPath, setIsNewsPath] = useState<boolean | null>(
     checkIfNewsPath || null
   );
+  const [isLookbookPath, setIsLookbookPath] = useState<boolean | null>(
+    checkIfLookbookPath || null
+  );
 
   useEffect(() => {
     setIsShopPath(checkIfShopPath());
     setIsNewsPath(checkIfNewsPath());
+    setIsLookbookPath(checkIfLookbookPath());
   }, [router.pathname, checkIfShopPath]);
 
   const navigateToShop = () => router.push("/shop-2");
   const navigateToNews = () => router.push("/news");
+  const navigateToLookbook = () => router.push("/lookbooks");
+
+  // const navOpenAndSiteLoading = !navOpen && !isLoading
 
   return (
     <AnimatePresence>
-      {isShopPath && !navOpen && (
+      {isShopPath && !navOpen && !isLoading && (
         <HeaderBackButton
           onClick={navigateToShop}
           {...headerBackButtonVariants()}
@@ -34,10 +48,17 @@ const BackButton = ({ navOpen }: { navOpen: boolean }) => {
           back
         </HeaderBackButton>
       )}
-
-      {isNewsPath && !navOpen && (
+      {isNewsPath && !navOpen && !isLoading && (
         <HeaderBackButton
           onClick={navigateToNews}
+          {...headerBackButtonVariants()}
+        >
+          back
+        </HeaderBackButton>
+      )}
+      {isLookbookPath && !navOpen && !isLoading && (
+        <HeaderBackButton
+          onClick={navigateToLookbook}
           {...headerBackButtonVariants()}
         >
           back
